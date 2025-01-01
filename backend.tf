345abab282e934a8e02aa8aa519aff5b7dfc328f
Mohammed Hussain Shareef <114469176+hussainshareef@users.noreply.github.com>
2025-01-01 13:30:57 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,7 @@
+backend "azurerm" {
+    resource_group_name  = "Shareef-RG"
+    storage_account_name = "backedtfshareefstorage"
+    container_name       = "tfstate-shareef"
+    key                  = "terraform.tfstate"
+  }
+}
